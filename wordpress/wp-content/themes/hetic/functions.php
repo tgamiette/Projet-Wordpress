0fcd7a586da1b73bfa849e4e848728a2d6b2eeed
Maelys chassin <m.chassin@altelis.com>
2022-02-24 18:04:00 +0100

    Add custom postt type + metabox + show post

--- wordpress/wp-content/themes/hetic/functions.php
+++ wordpress/wp-content/themes/hetic/functions.php
@@ -20,6 +20,7 @@ function custom_setup_theme(){
     //add_theme_support('is_validate');
 }
 
+<<<<<<< HEAD
 // On ajoute une sidebar
 function wpbootstrap_sidebar(){
     register_sidebar(['name'          => "Sidebar principale",
@@ -86,10 +87,78 @@ function cptui_register_my_cpts_logement(){
                  'hierarchical'      => true,
                  'show_in_rest'      => true,
                  'show_admin_column' => true];
+=======
+add_action('after_setup_theme', 'wpbootstrap_after_setup_theme');
+
+//Ajout de Boostrap;
+add_action('wp_enqueue_scripts', 'wpheticBootstrap');
+function wpheticBootstrap()
+{
+    wp_enqueue_style('bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    wp_enqueue_script("bootstrap_js", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", [], false, true);
+}
+
+// On ajoute une sidebar
+function wpbootstrap_sidebar() {
+  register_sidebar([
+                     'name'          => "Sidebar principale",
+                     'id'            => 'main-sidebar',
+                     'description'   => "La sidebar principale",
+                     'before_widget' => '<div id="%1$s" class="widget %2$s p-4">',
+                     'after_widget'  => '</div>',
+                     'before_title'  => '<h4 class="widget-title font-italic">',
+                     'after_title'   => '</h4>',
+                   ]);
+}
+
+add_action('widgets_init', 'wpbootstrap_sidebar');
+
+
+//Ajout de widget "Logement"
+function cptui_register_my_cpts_logement()
+{
+    $labels = [
+        "name" => __("Logements", "custom-post-type-ui"),
+        "singular_name" => __("Logement", "custom-post-type-ui"),
+    ];
+
+    $args = [
+        "label" => __("Logements", "custom-post-type-ui"),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "show_in_rest" => true,
+        "has_archive" => true,
+        "delete_with_user" => false,
+        "capability_type" => "post",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => ["slug" => "logement", "with_front" => true],
+        "query_var" => true,
+        "supports" => ["title", "thumbnail"],
+        "show_in_graphql" => false,
+    ];
+
+    register_post_type("logement", $args);
+
+    $labelsTaxo = [
+        'name' => 'Styles',
+        'singular_name' => 'Style'
+    ];
+
+    $argsTaxo = [
+        'labels' => $labelsTaxo,
+        'public' => true,
+        'hierarchical' => true,
+        'show_in_rest' => true,
+        'show_admin_column' => true
+    ];
+>>>>>>> 5e725fa (Add custom postt type + metabox + show post)
 
     register_taxonomy('style', ['post'], $argsTaxo);
 }
 
+<<<<<<< HEAD
 //Add metabox to post Logements
 function hcf_register_meta_boxes(){
     add_meta_box('hcf-1', __('Desciption du logement', 'hcf'), 'hcf_display_callback', 'logement');
@@ -100,6 +169,22 @@ function hcf_display_callback($post){
 }
 
 add_action('add_meta_boxes', 'hcf_register_meta_boxes');
+=======
+add_action('init', 'cptui_register_my_cpts_logement');
+
+
+
+//Add metabox to post Logements
+function hcf_register_meta_boxes() {
+    add_meta_box( 'hcf-1', __( 'Desciption du logement', 'hcf' ), 'hcf_display_callback', 'logement' );
+}
+
+function hcf_display_callback( $post ) {
+    include plugin_dir_path( __FILE__ ) . './metabox.php';
+}
+
+add_action( 'add_meta_boxes', 'hcf_register_meta_boxes' );
+>>>>>>> 5e725fa (Add custom postt type + metabox + show post)
 
 
 /**
@@ -107,6 +192,7 @@ add_action('add_meta_boxes', 'hcf_register_meta_boxes');
  *
  * @param int $post_id Post ID
  */
+<<<<<<< HEAD
 function hcf_save_meta_box($post_id){
     if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
         return;
@@ -151,3 +237,29 @@ add_action('init', 'cptui_register_my_cpts_logement');
 add_action('save_post', 'hcf_save_meta_box');
 
 
+=======
+function hcf_save_meta_box( $post_id ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+    $fields = [
+        'hcf-description',
+        'hcf-logement_type',
+        'hcf-espace',
+        'hcf-nb_lit',
+        'hcf-nb_sdb',
+        'hcf-nb_pers',
+        'hcf-adresse_logement',
+        'hcf-ville_logement',
+        'hcf-prix_logement',
+        'hcf-proprio_type',
+    ];
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST ) ) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+        }
+     }
+}
+add_action( 'save_post', 'hcf_save_meta_box' );
+>>>>>>> 5e725fa (Add custom postt type + metabox + show post)
