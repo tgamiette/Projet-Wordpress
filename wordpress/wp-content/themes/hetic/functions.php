c5f8aee8940ab13a6c888f96b6f5a0fa061fc2dd
Mehdi Saoud <saoudmehdi5@gmail.com>
2022-02-24 15:53:18 +0100

    Add header

--- wordpress/wp-content/themes/hetic/functions.php
+++ wordpress/wp-content/themes/hetic/functions.php
@@ -3,14 +3,31 @@
 
 
 function wpbootstrap_after_setup_theme() {
-  // On ajoute un menu
-  register_nav_menu('header_menu', "Menu du header");
-  // On ajoute une classe php permettant de gérer les menus Bootstrap
-//  require_once get_template_directory() . 'class-wp-bootstrap-navwalker.php';
 
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header_menu', "Menu du header");
 }
-
 add_action('after_setup_theme', 'wpbootstrap_after_setup_theme');
+
+function wpheticBootstrap()
+{
+    wp_enqueue_style('bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    wp_enqueue_script("bootstrap_js", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", [], false, true);
+}
+add_action('wp_enqueue_scripts', 'wpheticBootstrap');
+
+add_filter('nav_menu_css_class', function ($classes) {
+    $classes[] = "nav-item";
+    return $classes;
+});
+
+add_filter('nav_menu_link_attributes', function ($attr) {
+    $attr['class'] = 'nav-link';
+    return $attr;
+});
+
 
 // On ajoute une sidebar
 function wpbootstrap_sidebar() {
@@ -26,3 +43,4 @@ function wpbootstrap_sidebar() {
 }
 
 add_action('widgets_init', 'wpbootstrap_sidebar');
+
--- wordpress/wp-content/themes/hetic/header.php
+++ wordpress/wp-content/themes/hetic/header.php
@@ -10,34 +10,23 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div class="container">
-  <!-- Header -->
-  <header class="blog-header py-3">
-    <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-12 text-center">
-        <a class="blog-header-logo text-dark" href="<?php bloginfo('url'); ?>"><?php bloginfo('name') ?></a>
-      </div>
-    </div>
-  </header>
-  <!-- Fin du header -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?php wp_nav_menu([
+                'theme_location' => 'header_menu',
+                'container' => false,
+                'menu_class' => "navbar-nav me-auto mb-2 mb-lg-0"
+            ]); ?>
 
-  <!-- Menu header -->
-<!--  <nav class="navbar navbar-expand-md navbar-light" role="navigation">-->
-<!--    <div class="container">-->
-<!--      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-menu" aria-controls="#header-menu" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--        <span class="navbar-toggler-icon"></span>-->
-<!--      </button>-->
-<!--      --><?php
-//      wp_nav_menu(array(
-//                    'theme_location'    => 'header_menu',
-//                    'depth'             => 2,
-//                    'container'         => 'div',
-//                    'container_class'   => 'collapse navbar-collapse',
-//                    'container_id'      => 'header-menu',
-//                    'menu_class'        => 'nav navbar-nav',
-////                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-////                    'walker'            => new WP_Bootstrap_Navwalker(),
-//                  ));
-//      ?>
-<!--    </div>-->
-<!--  </nav>-->
+            <?php get_search_form(); ?>
+
+        </div>
+    </div>
+</nav>
+</body>
