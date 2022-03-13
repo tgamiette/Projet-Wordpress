5324ade8068dda171dbc988d4398bc8318be959d
teddy <tgamiette@kernix.com>
2022-03-13 10:53:29 +0100

    maj

--- wordpress/wp-content/plugins/logement-manager/wp_logement-plugin.php
+++ wordpress/wp-content/plugins/logement-manager/wp_logement-plugin.php
@@ -10,35 +10,34 @@ Author: Maélys
 
 */
 
-register_activation_hook( __FILE__ , function(){
+register_activation_hook(__FILE__, function () {
 
-  $admin = get_role('administrator');
-  $admin->add_cap('manage_logement');
+    $admin = get_role('administrator');
+    $admin->add_cap('manage_logement');
 
 
-  add_role('logement_manager', 'Logement Manager', [
-    'read' => true,
-    'manage_logement' => true
-  ]);
+    add_role('logement_manager', 'Logement Manager', [
+        'read' => true,
+        'manage_logement' => true]);
 });
 
-register_deactivation_hook( __FILE__ , function(){
-  $admin = get_role('administrator');
-  $admin->remove_cap('manage_logement');
-  remove_role('logement_manager');
+register_deactivation_hook(__FILE__, function () {
+    $admin = get_role('administrator');
+    $admin->remove_cap('manage_logement');
+    remove_role('logement_manager');
 });
 
 
 //Form add logement post
 require_once 'classes/wp-addPost.php';
 
-add_shortcode('add_logement', function(){
-  $manager = new Wp_addPost();
-  return $manager->generateForm();
+add_shortcode('add_logement', function () {
+    $manager = new Wp_addPost();
+    return $manager->generateForm();
 });
 
-add_action('admin_post_hcf_logement_post', function(){
-  Wp_addPost::handleForm();
+add_action('admin_post_hcf_logement_post', function () {
+    Wp_addPost::handleForm();
 });
 
 ?>
